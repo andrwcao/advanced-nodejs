61224c5bd8cbc46b0a4dfef5652a86980808b3b3
andrwcao <80874259+andrwcao@users.noreply.github.com>
2023-01-18 18:40:00 +1100

    Display blog's image and handle null image case

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -14,15 +14,17 @@ export const handleToken = token => async dispatch => {
 };
 
 export const submitBlog = (values, file, history) => async dispatch => {
-  const uploadConfig = await axios.get('/api/upload');
-  await axios.put(uploadConfig.data.url, file, {
-    headers: {
-      'Content-Type': file.type
-    }
-  });
-
-  const res = await axios.post('/api/blogs', {...values, imageUrl: uploadConfig.data.key});
-
+  if (file != null) {
+    const uploadConfig = await axios.get('/api/upload');
+    await axios.put(uploadConfig.data.url, file, {
+      headers: {
+        'Content-Type': file.type
+      }
+    });
+  
+    const res = await axios.post('/api/blogs', {...values, imageUrl: uploadConfig.data.key});
+  }
+  const res = await axios.post('/api/blogs', {...values, imageUrl: null});
   history.push('/blogs');
   dispatch({ type: FETCH_BLOG, payload: res.data });
 };
